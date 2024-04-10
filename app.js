fe7cda2ea0560f5ec1d3951bcf8d4831f45dcc26
DESKTOP-S7FGUGR\Rohit Das <rohitd3476@gmail.ccom>
2024-04-09 23:28:57 -0700

    Added connect-flash, multer, fixed PUT & DELETE method and added partials for header/footer

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const { spawn } = require('child_process');
 const path = require('path');
+const bodyParser = require("body-parser");
+const flash = require("connect-flash");
+const multer = require("multer")
 const session = require('express-session');
 const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./app/public"));
 app.use(express.json());
+app.use(flash())
 
 app.use(session({
   secret: 'your secret key',
@@ -18,12 +22,25 @@ app.use(session({
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+app.use((req, res, next)=>{
+    res.locals.error = req.flash('error')
+    res.locals.success = req.flash('success')
+    if(req.query._method == 'DELETE'){
+      req.method = "DELETE"
+      req.url = req.path
+    } else if(req.query._method == 'PUT'){
+      req.method = "PUT"
+      req.url = req.path
+    }
+    next()
+})
+
 app.get("/", (req, res) => {
     res.render("home", { uname: "" });
 })
 
 app.post("/", (req, res) => {
-    const { uname } = req.params;
+    const { uname } = req.body;
     res.render("home", { uname: uname })
     
     // res.redirect("/")
